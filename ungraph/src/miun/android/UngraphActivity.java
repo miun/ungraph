a20a12e2ae6231ea95d6a2dbc2577ae537ca44f7
tisc <t.schroeder84@web.de>
2012-02-07 16:33:17 +0100

    first camera functions added

--- ungraph/src/miun/android/UngraphActivity.java
+++ ungraph/src/miun/android/UngraphActivity.java
@@ -1,13 +1,40 @@
 package miun.android;
 
+import miun.android.R.id;
 import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.FrameLayout;
 
 public class UngraphActivity extends Activity {
-    /** Called when the activity is first created. */
+	private Camera mCamera;
+	private CameraPreview mPreview;
+	
+	//Return available camera or null
+	private Camera getFirstCam() {
+		try {
+			return Camera.open();
+		}
+		catch (Exception e) {
+			Log.e("ungraph.cam","Cannot open camera");
+			return null;
+		}
+	}
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        //Camera tests
+        mCamera = getFirstCam();
+        
+        if(mCamera != null) {
+        	mPreview = new CameraPreview(this,mCamera);
+        	FrameLayout preview = (FrameLayout) findViewById(id.camera_preview);
+        	preview.addView(mPreview);
+        }
     }
 }
